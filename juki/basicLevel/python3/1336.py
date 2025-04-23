88d520db767c4954c95c4434aa81eb3ce8675def
Bryan Miranda Palacios <105233978+bmirandap@users.noreply.github.com>
2025-04-23 12:00:26 -0400

    Delete juki/basicLevel directory

--- juki/basicLevel/python3/1336.py
+++
@@ -1,8 +0,0 @@
-# Problem ID: 1336
-# Title: Imprimir Python
-# Last modification: 09-May-2022
-print("PPPPPP  Y     Y  TTTTTT  H     H  OOOOOO  N   N")
-print("P    P   Y   Y     T     H     H  O    O  NN  N")
-print("PPPPPP    Y Y      T     HHHHHHH  O    O  N N N")
-print("P          Y       T     H     H  O    O  N  NN")
-print("P          Y       T     H     H  OOOOOO  N   N")
--- juki/basicLevel/python3/1342.py
+++
@@ -1,5 +0,0 @@
-# Problem ID: 1342
-# Title: imprime HOLA MUNDO
-# Last modification: 09-May-2022
-n = int(input())
-print("Hola Mundo")
